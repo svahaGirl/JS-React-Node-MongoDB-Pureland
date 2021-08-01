62cdc3f29881e987872b2780cddc1484a0f78d86
Ying - a buddhist developer <fyanton@hotmail.com>
2021-08-01 11:40:20 -0700

    Add Router to screens
    Add browser router to homescreen and productscreen.

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Product from './components/Product';
-import data from './data';
+import {BrowserRouter, Route} from 'react-router-dom'; // must use {} to import this.
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
-    function App() {
+function App() {
     return (
+        <BrowserRouter>
         <div className="grid-container">
         <header className="row">
             <div>
@@ -16,21 +18,15 @@ import data from './data';
         </header>
 
         <main>
-        <div>
-            <div className="row center">
-            {
-                data.products.map((product) => (
-                    <Product key={product._id} product={product}></Product>
-                ))
-            }         
-            </div>
-            </div>
+            <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>        
         </main>
         <footer className="row center">
             All right reserved
         </footer>
     </div>
+</BrowserRouter>
     );
     }
 
-    export default App;
+export default App;
