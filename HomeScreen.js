4f539d1a0d4e74e62fcbef4ef10fc4479435ae9c
Ying - a buddhist developer <fyanton@hotmail.com>
2021-08-01 11:37:04 -0700

    Create HomeScreen.js
    create a folder: screens, HomeScreen.js

---
+++ HomeScreen.js
@@ -0,0 +1,47 @@
+//create folder screens
+// use rfc to auto generate class
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Product from '../components/Product';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+//import data from '../data';
+
+export default function HomeScreen() {
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                setLoading(true);
+                const {data} = await axios.get('/api/products');
+                setLoading(false);
+                setProducts(data);
+            } catch(err){
+                setError(err.message);
+                setLoading(false);
+            }
+            
+        };
+        fetchData();
+    }, []);
+    return (       
+        <div>
+            {loading ?(
+                <LoadingBox></LoadingBox>
+            ) : error ? (
+                <MessageBox variant="danger">{error}</MessageBox>
+            ) : (
+                <div className="row center">
+            {
+                products.map((product) => (
+                    <Product key={product._id} product={product}></Product>
+                ))
+            }         
+            </div>
+            )}                        
+        </div>   
+    );
+}
