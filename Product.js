ca12c74ca468532f3df781af5d4f9bd5f18eef9c
Ying - a buddhist developer <fyanton@hotmail.com>
2021-08-01 13:50:13 -0700

    Fixed line 15 and line 18, should be ` `
    Fixed line 15 and 18, should be ` `, for the href.....

--- Product.js
+++ Product.js
@@ -1,7 +1,10 @@
 // add components folder, and Product.js
+// in line 
 
 import React from 'react';
 import Rating from './Rating';
+
+//line 15,`/product/${product._id}`: is ` the one next to 1,  not '
 
 export default function Product(props){
 
@@ -9,19 +12,17 @@ export default function Product(props){
 
     return( <div key={product._id} className="card">
 
-                <a href={'/product/${product._id}'}>
-                
+                <a href={`/product/${product._id}`}>                
                 <img className="medium" src={product.image} alt={product.name}/></a>
                     <div className="card-body">
-                    <a href={'/product/${product._id}'}>
+                    <a href={`/product/${product._id}`}>
                             <h2>{product.name}</h2>
                         </a>
                         <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
-
                         <div className="price">
                         ${product.price}
                         </div>
                     </div> 
-                    </div>
-                    )
+            </div>
+        );
 }
