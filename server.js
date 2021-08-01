c04c6cc356d03d986171f9965df16e5fb4167099
Ying - a buddhist developer <fyanton@hotmail.com>
2021-08-01 11:27:28 -0700

    Create server.js
    server.js, set port to 5000

---
+++ server.js
@@ -0,0 +1,16 @@
+//Create backend folder and server.js
+
+
+const app = express();
+
+    app.get('/api/products', (req, res) => {
+    res.send(data.products);
+    });
+
+    app.get('/', (req, res) => {
+    res.send('Server is ready');
+    });
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+    });
