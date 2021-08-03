8922a1e543a36fd1777dd52f57143052c9bfcdd4
Ying - a buddhist developer <fyanton@hotmail.com>
2021-08-03 10:20:48 -0700

    Add detailsReducer
    Add detailsReducer

--- productReducers.js
+++ productReducers.js
@@ -17,3 +17,18 @@ export const productListReducer = (state = {loading: true, products: []}, action
             return state;
     }
 };
+
+
+export const productDetailsReducer = (state ={ product:{}, loading: true}, action) => {
+
+    switch (action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return {loading: true};
+        case PRODUCT_DETAILS_SUCCESS:
+            return {loading: false, product: action.payload};
+        case PRODUCT_DETAILS_FAIL:
+            return {loading: false, error: action.payload};
+        default:
+            return state;
+    }
+};
