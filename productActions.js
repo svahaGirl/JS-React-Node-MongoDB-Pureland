4020652ff5e008ee0164eb10063bbab5b6eb03f0
Ying - a buddhist developer <fyanton@hotmail.com>
2021-08-03 09:24:45 -0700

    Add detailsProduct
    Add product details function

--- productActions.js
+++ productActions.js
@@ -14,3 +14,22 @@ export const listProducts = () => async (dispatch) => {
     }
 
 };
+
+export const detailsProduct = (productId) => async (dispatch) =>{
+
+    dispatch({
+        type: PRODUCT_DETAILS_REQUEST, payload: productId
+    });
+    try{
+        const{data} = await axios.get(`/api/products/${productId}`);
+        dispatch({type: PRODUCT_DETAILS_SUCCESS, payload: data});
+    } catch (error){
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
+            payload:
+            error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+        });
+    }
+};
